7dcceb5267c1c554e8500ea082b60a8a53bb358d
AlexCLeduc <ledualex@gmail.com>
2022-10-04 17:28:25 -0400

    bump major version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="titan-zeus",
-    version="1.1.0",
+    version="2.0.0",
     author="AlexCLeduc",
     # author_email="author@example.com",
     # description="A small example package",
